5e305a69b1741d9409623c988971459751bb0a08
Felipe R. de Luca <feliper.deluca@Felipes-MacBook-Pro-4.local>
2023-06-11 23:36:15 -0300

    Inicio construcao Q learning

--- pitfallbot.py
+++ pitfallbot.py
@@ -8,11 +8,16 @@
 # References and credits
 #   Gymnasium:
 #       https://gymnasium.farama.org/content/basic_usage/
+#       https://gymnasium.farama.org/tutorials/training_agents/blackjack_tutorial/
 #
 #   Digitalocean:
 #       https://www.digitalocean.com/community/tutorials/how-to-build-atari-bot-with-openai-gym
+#
+#   Real Python:
+#       https://realpython.com/python-bitwise-operators/
 
 import gymnasium as gym
+import numpy as np
 
 env = gym.make("ALE/Pitfall-v5", render_mode="human", obs_type="ram")
 env.action_space = gym.spaces.Discrete(18)
@@ -38,24 +43,38 @@ action_list = {
     17: "DOWNLEFTFIRE"
 }
 
+
+class PitfallAgent:
+    pass
+
+
+def bit_convert(n):
+    out = 0
+    for bit in n:
+        out = (out << 1) | bit
+    return out
+
 def main():
-    observation, info = env.reset(seed=10)
+    game_state, info = env.reset(seed=10)
     # Configura a recompensa
     episode_reward = 0
     rewards = []
 
+    num_episodes = 1000
     # Looping do jogo com duração estabelecida
-    for _ in range(1000):
-        
+    for _ in range(num_episodes):
         action = env.action_space.sample()
         # Realiza uma ação no ambiente
-        observation, reward, terminated, truncated, info = env.step(action)
+        new_game_state, reward, terminated, truncated, info = env.step(action)
 
+        game_state_id = bit_convert(game_state)
+        game_state = new_game_state
         # Imprime a recompensa de acordo com a ação atual
         reward_sign = "+"
         if reward < 0:
             reward_sign = "-"
-        print(f'{reward_sign} Reward: {reward} -> current action: {action_list[action]}')
+        
+        print(f'{reward_sign} Reward: {reward:6} -> {action_list[action]:15} Game state: {game_state_id}')
 
         episode_reward += reward
         if terminated or truncated:
